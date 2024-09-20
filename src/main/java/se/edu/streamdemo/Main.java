f9a3aa1c27a620a013af71fd2073c6bec8dc0c34
Akshay Narayan <okkhoy@gmail.com>
2024-09-20 14:29:36 +0800

    Add methods to work with deadlines

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -1,6 +1,7 @@
 package se.edu.streamdemo;
 
 import se.edu.streamdemo.data.Datamanager;
+import se.edu.streamdemo.task.Deadline;
 import se.edu.streamdemo.task.Task;
 
 import java.util.ArrayList;
@@ -10,12 +11,38 @@ public class Main {
         System.out.println("Welcome to Task manager (using streams)");
         Datamanager dataManager = new Datamanager("./data/data.txt");
         ArrayList<Task> tasksData = dataManager.loadData();
+
+        System.out.println("Printing all data ...");
         printAllData(tasksData);
+
+        System.out.println("Printing deadlines ...");
+        printDeadlines(tasksData);
+
+        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+
+    }
+
+    private static int countDeadlines(ArrayList<Task> tasksData) {
+        int count = 0;
+        for (Task t : tasksData) {
+            if (t instanceof Deadline) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public static void printAllData(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             System.out.println(t);
+        }
+    }
+
+    public static void printDeadlines(ArrayList<Task> tasksData) {
+        for (Task t : tasksData) {
+            if (t instanceof Deadline) {
+                System.out.println(t);
+            }
         }
     }
 
